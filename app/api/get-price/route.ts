1b60bc1310b7a0a39f4dbf730e24694bfac2025c
Rosalyn van Zyl <125730414+RosSuhan@users.noreply.github.com>
2025-11-26 13:35:41 +0200

    get-price2
    more console logs in get-price

--- app/api/get-price/route.ts
+++ app/api/get-price/route.ts
@@ -6,16 +6,23 @@ export async function GET(request:Request) {
     const pickup = searchParams.get("pickup");
     const dropoff = searchParams.get("dropoff")
 
+    console.log("API Route input:", {pickup, dropoff});
+
     const RATES_GOOGLE_SCRIPT_URL = process.env.RATES_GOOGLE_SCRIPT_URL!;
 
     try {
-        const response = await fetch(
-            `${RATES_GOOGLE_SCRIPT_URL}?pickup=${pickup}&dropoff=${dropoff}`
-        );
+        const url = `${RATES_GOOGLE_SCRIPT_URL}?pickup=${pickup}&dropoff=${dropoff}`;
+        console.log("FETCHING GOOGLE SCRIPT:", url);
+
+        const response = await fetch(url);
+        console.log("GOOGLE SCRIPT STATUS:", response.status)
 
         const data = await response.json();
+        console.log("GOOGLE SCRIPT DATA:", data);
+
         return NextResponse.json(data);
     } catch (err) {
+        console.error("API error:", err);
         return NextResponse.json({ error: "Failed to fetch price" }, { status: 500 });
     }
 }
